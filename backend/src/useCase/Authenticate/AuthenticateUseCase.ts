5a8129c043e341cbd956c98afed12671fe0f5d43
fabiano-debugador <fabiano_the_beast@hotmail.com>
2022-10-16 07:26:48 -0300

    changed usage of token key in authentication

--- backend/src/useCase/Authenticate/AuthenticateUseCase.ts
+++ backend/src/useCase/Authenticate/AuthenticateUseCase.ts
@@ -1,7 +1,12 @@
 import { sign } from "jsonwebtoken";
 import { IAuthenticateRepository } from "../../repositories/IAuthenticateRepository";
 import { IAuthenticateDTO } from "./AuthenticateDTO";
+import * as dotenv from "dotenv";
 
+dotenv.config();
+
+const tokenKey =
+  process.env.TOKEN_KEY || "13f3b6cb-3f98-4258-93c7-82878099f1d9";
 export class AuthenticateUseCase {
   constructor(private authenticateRepository: IAuthenticateRepository) {}
 
@@ -15,7 +20,7 @@ export class AuthenticateUseCase {
       throw new Error("Name or password incorrect");
     }
 
-    const token = sign({}, "13f3b6cb-3f98-4258-93c7-82878099f1d9", {
+    const token = sign({}, tokenKey, {
       subject: userExist.id,
       expiresIn: "36000000s",
     });
